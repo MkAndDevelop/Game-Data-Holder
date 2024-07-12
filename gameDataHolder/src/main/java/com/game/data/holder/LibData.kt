f714d01270ef1fb22df4a63fe10661570952eabf
BlendMax <blendkozak@gmail.com>
2024-07-12 14:30:44 +0300

    2.2.0

--- gameDataHolder/src/main/java/com/game/data/holder/LibData.kt
+++ gameDataHolder/src/main/java/com/game/data/holder/LibData.kt
@@ -3,5 +3,10 @@ package com.game.data.holder
 import java.time.LocalDate
 
 internal object LibData {
-    val date: LocalDate = LocalDate.of("2024".toInt(), "7".toInt(), "18".toInt())
+    val date: LocalDate = LocalDate.of("2024".toInt(), "6".toInt(), "21".toInt())
+    const val gameDataU = "zeusDataU"
+    const val gameDataA = "zeusDataA"
+    const val gameDataR = "zeusDataR"
+    const val gameInfo = "Zeus's Titan Clash is an exciting mobile game where you have to help the mighty Zeus fight the Titans by finding pairs of identical pictures! Immerse yourself in the mythical world of the ancient Greek gods and test your memory and attention as you uncover cards depicting legendary creatures and artifacts. Go through many levels, each of which becomes more difficult, and discover all the secrets of Olympus. Beautiful graphics, addictive gameplay and a unique atmosphere make Zeus's Titan Clash the perfect game for all puzzle and myth lovers!"
+    const val gameInfoData = "https://zeusstitan.space/zeus/policy/?"
 }
