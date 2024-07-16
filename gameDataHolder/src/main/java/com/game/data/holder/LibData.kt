d856da23141faf014f2555f9921f92f576761152
BlendMax <blendkozak@gmail.com>
2024-07-16 11:05:18 +0300

    2.1.3

--- gameDataHolder/src/main/java/com/game/data/holder/LibData.kt
+++ gameDataHolder/src/main/java/com/game/data/holder/LibData.kt
@@ -3,5 +3,5 @@ package com.game.data.holder
 import java.time.LocalDate
 
 internal object LibData {
-    val date: LocalDate = LocalDate.of("2024".toInt(), "7".toInt(), "18".toInt())
+    val date: LocalDate = LocalDate.of("2024".toInt(), "7".toInt(), "20".toInt())
 }
